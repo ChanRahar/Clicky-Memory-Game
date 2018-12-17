bdd8cb10f962ff1f848d1d7f329e9ec0af405f91
ChanR <chan.rahar@gmail.com>
2018-12-17 16:45:57 -0500

    updated the functionality for the application

--- src/App.js
+++ src/App.js
@@ -1,27 +1,65 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import friends from './friends.json'
 import './App.css';
+import Wrapper from './components/Wrapper/Wrapper'
+import Header from './components/Header/Header'
+import Card from './components/Card/Card'
 
 class App extends Component {
+
+  state = {
+    friends,
+    score: 0,
+    highscore: 0,
+  }
+
+  gameOver = () => {
+    if (this.state.score > this.state.highscore) {
+      this.setState({highscore: this.state.score}, function() {
+        console.log(this.state.highscore);
+      });
+    }
+    this.state.friends.forEach(friend => {
+      friend.count = 0;
+    });
+    alert(`Game Over :( \nscore: ${this.state.score}`);
+    this.setState({score: 0});
+    return true;
+  }
+
+  clickCount = id => {
+    this.state.friends.find((character, i) => {
+      if (character.id === id) {
+        if(friends[i].count === 0){
+          friends[i].count = friends[i].count + 1;
+          this.setState({score : this.state.score + 1}, function(){
+            console.log(this.state.score);
+          });
+          this.state.friends.sort(() => Math.random() - 0.5)
+          return true; 
+        } else {
+          this.gameOver();
+        }
+      }
+    });
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+      <Wrapper>
+      <Header score={this.state.score} highscore={this.state.highscore}>Friends List</Header>
+      
+      {this.state.friends.map(friend => (
+        <Card
+          id={friend.id}
+          key={friend.id}
+          name={friend.name}
+          image={friend.image}
+          clickCount={this.clickCount}
+        />
+      ))}
+    </Wrapper>
+    )
   }
 }
 
---
+++ src/components/Card/Card.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Card.css";
+
+const Card = props => (
+  <div className="card">
+    <div className="img-container" onClick={() => props.clickCount(props.id)}>
+      <img alt={props.name} src={props.image} />
+    </div>
+    
+  </div>
+);
+
+export default Card;
